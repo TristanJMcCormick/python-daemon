e6cd2eed269c4099fbf8e6ab49735279ff2cfca4
Tristan McCormick <tristanjmccormick@gmail.com>
2016-01-09 18:17:09 -0500

    Break out text parsing

--- txt2do/daemon/views.py
+++ txt2do/daemon/views.py
@@ -44,62 +44,69 @@ SMS_TEMPLATES = {
 @require_http_methods(["POST"])
 def task(request):
     incoming_text = Text.objects.create(
-    body = request.POST.get('Body', 'No message body').lower(),
-    message_sid = request.POST.get('MessageSid'),
-    from_number = request.POST.get('From'),
-    to_number = request.POST.get('To'),
+        body = request.POST.get('Body', 'No message body').lower(),
+        message_sid = request.POST.get('MessageSid'),
+        from_number = request.POST.get('From'),
+        to_number = request.POST.get('To'),
     )
     foursquare_client = FoursquareClient()
     twilio_client = TwilioClient()
     # Split out the pre-flag text, this will indicate the task to accomplish and any required keywords
-    try:
-        query_parts = incoming_text.body.lower().split('-')
-        task_type = query_parts[0].split(' ', 1)[0]
-        task_query = query_parts[0].split(' ', 1)[1]
-        flags = query_parts[1:]
-    except IndexError as ex:
-        twilio_client.send_sms(settings.TWILIO_NUMBER,incoming_text.from_number,SMS_TEMPLATES['WRONG_TASK_TYPE'])
-        return HttpResponseBadRequest("WRONG_TASK_TYPE")
+    parsed_task = _parse_task_parts(incoming_text)
     #Handle foursquare task
-    if task_type == 'foursquare':
-        if not task_query:
+    if parsed_task['task_type'] == 'foursquare':
+        if not parsed_task['task_query']:
             twilio_client.send_sms(settigns.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['MALFORMED_QUERY']['foursquare'])
             return HttpResponseBadRequest("Query malformed")
         near_param = ''
         #Right now only handle a "near" parameter that specifies a neighborhood.
-        if flags and 'n ' in flags[0]:
-            near_param = flags[0][1:].strip()
-        try:
-            query_with_options = {'query':task_query, 'near':settings.LOCATION if not near_param else near_param}
-            venue_details_response = foursquare_client.query_foursquare_venues(query_with_options)
-            if venue_details_response.get('venues') and venue_details_response.get('venues')[0]:
-                contact = venue_details_response.get('venues')[0].get('contact')
-                location = venue_details_response.get('venues')[0].get('location')
-                if location.get('formattedAddress'):
-                    sms_body = SMS_TEMPLATES['QUERY_RESPONSE']['foursquare'].format(' '.join(location.get('formattedAddress', 'No Address')) + ', ' + contact.get('phone','No phone'))
-                    twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, sms_body)
-                    return HttpResponse("Success")
-                else:
-                    sms_body = SMS_TEMPLATES['QUERY_RESPONSE']['foursquare'].format(location.get('address','No Address') + ', at ' + location.get('crossStreet','No cross street') + ', ' + contact.get('phone','No phone'))
-                    twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, sms_body)
-                    return HttpResponse("Success")
+        if flags and 'n ' in parsed_task['flags']:
+            near_param = parsed_task['flags']['-n']
+        query_with_options = {'query':parsed_task['task_query'], 'near':settings.LOCATION if not near_param else near_param}
+        venue_details_response = foursquare_client.query_foursquare_venues(query_with_options)
+        if venue_details_response.get('venues') and venue_details_response.get('venues')[0]:
+            contact = venue_details_response.get('venues')[0].get('contact')
+            location = venue_details_response.get('venues')[0].get('location')
+            if location.get('formattedAddress'):
+                sms_body = SMS_TEMPLATES['QUERY_RESPONSE']['foursquare'].format(' '.join(location.get('formattedAddress', 'No Address')) + ', ' + contact.get('phone','No phone'))
+                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, sms_body)
+                return HttpResponse("Success")
             else:
-                if incoming_text.from_number is not settings.ADMIN_NUMBER:
-                    twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['NO_RESPONSE']['foursquare'])
-                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.ADMIN_NUMBER, SMS_TEMPLATES['NO_RESPONSE']['foursquare'])
-                return HttpResponse("Nothing returned for that query")
-
-        except EndpointError as ex:
+                sms_body = SMS_TEMPLATES['QUERY_RESPONSE']['foursquare'].format(location.get('address','No Address') + ', at ' + location.get('crossStreet','No cross street') + ', ' + contact.get('phone','No phone'))
+                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, sms_body)
+                return HttpResponse("Success")
+        else:
             if incoming_text.from_number is not settings.ADMIN_NUMBER:
-                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['MALFORMED_QUERY']['foursquare'])
-            twilio_client.send_sms(settings.TWILIO_NUMBER, settings.ADMIN_NUMBER, SMS_TEMPLATES['MALFORMED_QUERY']['foursquare'])
-            return HttpResponseBadRequest('Foursquare api is down or something is wrong with the query')
-        except InvalidAuth as ex:
-            if incoming_text.from_number is not settings.ADMIN_NUMBER:
-                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['ADMIN_NOTIFIED'])
-            twilio_client.send_sms(settings.TWILIO_NUMBER, settings.ADMIN_NUMBER, SMS_TEMPLATES['NOTIFY_ADMIN'])
-            return HttpResponseForbidden('Foursquare authorization failed')
+                twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['NO_RESPONSE']['foursquare'])
+            twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.ADMIN_NUMBER, SMS_TEMPLATES['NO_RESPONSE']['foursquare'])
+            return HttpResponse("Nothing returned for that query")
     else:
         twilio_client.send_sms(settings.TWILIO_NUMBER, incoming_text.from_number, SMS_TEMPLATES['WRONG_TASK_TYPE'])
         return HttpResponseNotFound("That task isn't supported yet. Just foursquare for now!")
     return HttpResponse('All branches should be handled, we shouldn\'t get here.')
+
+'''
+    Takes the new incoming text and produces a dictionary with three keys:
+        'task_type' : Which of the configured daemon behaviors this text is calling on
+            e.g. As of 1.9 that was only foursquare but budgeting is coming soon and
+            the options are endless
+        'query' : The text of the query for whatever service, task is being called
+            e.g. Brooklyn Bagel
+        'flags' : A dictionary of flags
+            e.g. -n Brooklyn or -d 3 for "near brooklyn" and "depth of 3"
+    Note that this method depends on the text being formatted as
+    "<keyword> <query> <-flag param -flag param...>" and will throw an error if
+    it doesn't
+'''
+def _parse_task_parts(incoming_text):
+    print('in parsed task parts')
+    sms_body = incoming_text.body.lower()
+    task_type = sms_body[:sms_body.find(' ')]
+    task_query = sms_body[sms_body.find(' '):sms_body.find('-')].strip()
+    flags_string = sms_body[sms_body.find('-'):]
+    flags_dict = []
+    print repr(flags_string)
+    return
+    # print('flags', flags)
+    parsed_sms = {'task_type' : task_type, 'query' : task_query, 'flags' : flags}
+    return parsed_sms
